dfc06388eb1a9661c3f878e31b2d46048a7d243b
Adil Seco <adil.seco@outlook.com>
2023-03-22 22:07:02 +0000

    added questions and moved file to correct git repo

---
+++ starter/assets/css/questions.js
@@ -0,0 +1,22 @@
+//questions
+var questions = [
+    {
+        questionTitle: "What's your favorite team",
+        choices: ['Benfica', 'Porto', 'Real Madrid'],
+        answer: 'Benfica';
+    }
+    
+    {
+        questionTitle: "Who's the best player ?",
+        choices: ['Tico Tico', 'Ronaldo', 'Sunil Chetri', 'Kyle Walker'],
+        answer: 'Ronaldo';
+    
+    
+    }
+    
+        questionTitle: "Who's the best player ?",
+        choices: ['Tico Tico', 'Ronaldo', 'Sunil Chetri', 'Kyle Walker'],
+        answer: 'Ronaldo';
+    ]
+    
+    
---
+++ starter/assets/css/script.js
@@ -0,0 +1,103 @@
+//Start Button
+1. Have a Event listener to for the START 
+2. create a questions function when user clicks on START
+2. Have a set setInterval in the button listener to start timer after 
+
+var startButtonEl = document.querySelector('start');
+var timerEl = document.querySelector('.timer');
+var finalscoreEl = document.querySelector('#final-score');
+var questionsEl = document.querySelector('#questions');
+var choicesEl = document.querySelector('#choices');
+var sumitEl - document.querySelector('#submit');
+var initialScoreEl = document.querySelector('#initials');
+var feedbackEl = document.querySelector('#feedback');
+
+var quizIndex = 0;
+var timer = questions.length * 5;
+var gameState = false;
+
+var sfxCorrect = new Audio('./assets/sfx/correct.wav');
+var sfxInorrect = new Audio('./assets/sfx/incorrect.wav');
+
+//function to start quiz
+
+function startQuiz 
+{
+    timerEl.textContent = timer;
+     
+    var startScreenEl = document.querySelector('#start-screen');
+    startScreenEl.setAttribute('class', 'hide');
+    questionsEl.removeAttribute('class');
+    timerID = setInterval(clockTick, 1000);
+
+    timerEl.textContent = 'timer';
+    
+
+    getQuestions();
+    console.log()
+}
+
+
+//function to get the questions
+function getQuestions()
+{
+    var currentQuestion = questions[quizIndex];
+    var questionTitle = document.querySelector('question-title');
+
+    questions.textContent = currentQuestion.questionTitle;
+
+    currentQuestion.choices.forEach(function(choice , i)
+    {
+        var choiceButton = document.createElement('button');
+        choiceButton.setAttribute('class', 'choices');
+
+        choiceButton.setAttribute('value', choice);
+        choiceButton.textContent = i + 1 + '.' + choice;
+
+        choicesEl.appendChild(choiceButton);
+    }
+        
+    });
+
+
+}
+//function for the question click
+
+
+//function to end quiz
+
+//function for the timer to start to count
+
+
+//function to save the high score
+
+
+//function to check if you pressed enter
+
+
+
+
+
+//questions
+var questions = [
+{
+    questionTitle: "What's your favorite team",
+    choices: ['Benfica', 'Porto', 'Real Madrid'],
+    answer: 'Benfica';
+}
+
+{
+    questionTitle: "Who's the best player ?",
+    choices: ['Tico Tico', 'Ronaldo', 'Sunil Chetri', 'Kyle Walker'],
+    answer: 'Ronaldo';
+
+
+}
+
+    questionTitle: "Who's the best player ?",
+    choices: ['Tico Tico', 'Ronaldo', 'Sunil Chetri', 'Kyle Walker'],
+    answer: 'Ronaldo';
+]
+
+
+
